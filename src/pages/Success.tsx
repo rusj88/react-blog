7df7bfa1aa92e38a1c6b9bd11316a665a705ab11
rusj88 <49836247+rusj88@users.noreply.github.com>
2022-11-23 15:17:44 +0300

    add success page

--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Success() {
   return (
-    <div className="min-h-[90vh] mt-6 mx-auto w-3/5 py-4 bg-white drop-shadow-xl rounded">
-      placeholder
+    <div className="w-96 mt-6 mx-auto bg-white rounded drop-shadow-xl">
+      <div className="flex flex-col items-center py-12 px-8">
+        <h2 className="text-lg">Account has been successfully created</h2>
+        <Link to="/sign-in">Log in</Link>
+      </div>
     </div>
   );
 }
