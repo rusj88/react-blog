4967d9c8ecea1ba577bda0e7a383451d2a057e95
rusj88 <49836247+rusj88@users.noreply.github.com>
2022-11-23 15:08:22 +0300

    add home link in nav

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Navigation() {
   return (
     <nav className="flex justify-between items-center h-16 w-full px-5 fixed top-0 left-0 shadow-md bg-gray-500 text-white z-10">
-      <h3>RealWorld Blog</h3>
+      <Link to={`/`}>RealWorld Blog</Link>
       <div className="flex gap-4">
         <Link to={`/sign-in`}>sign in</Link>
         <Link to={`/sign-up`}>sign up</Link>
