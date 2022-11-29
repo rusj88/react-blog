3c97459fab3efe5496d7651c0f7e6b6cf073d577
rusj88 <49836247+rusj88@users.noreply.github.com>
2022-11-29 16:04:56 +0300

    style

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,11 +2,25 @@ import { Link } from "react-router-dom";
 
 function Navigation() {
   return (
-    <nav className="flex justify-between items-center h-16 w-full px-5 fixed top-0 left-0 shadow-md bg-gray-500 text-white z-10">
-      <Link to={`/`}>RealWorld Blog</Link>
+    <nav className="flex justify-between items-center h-16 w-full px-5 fixed top-0 left-0 shadow-md bg-white z-10">
+      <Link to={`/`}>
+        <div className=" text-black text-lg">RealWorld Blog</div>
+      </Link>
       <div className="flex gap-4">
-        <Link to={`/sign-in`}>sign in</Link>
-        <Link to={`/sign-up`}>sign up</Link>
+        <Link to={`/sign-in`}>
+          <div className="w-24 h-12">
+            <div className="h-full flex justify-center items-center text-black text-lg">
+              Sign In
+            </div>
+          </div>
+        </Link>
+        <Link to={`/sign-up`}>
+          <div className="w-24 h-12 border-lime-500 border-2 rounded">
+            <div className="h-full flex justify-center items-center text-lime-500 text-lg">
+              Sign Up
+            </div>
+          </div>
+        </Link>
       </div>
     </nav>
   );
